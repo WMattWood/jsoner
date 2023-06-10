ec541f1d316e4dd095d6570d3e301b22224131b3
Matt Wood <w.matthew.wood@gmail.com>
2023-06-10 13:47:00 -0400

    Added check and error message for Ruby version.

--- jsoner.rb
+++ jsoner.rb
@@ -1,5 +1,16 @@
+# Check if Ruby version is up to date
+error_message = <<HEREDOC
+You are using a Ruby version below 2.5.0
+Please ensure you are using a more up to date Ruby version in order to use jsoner.rb
+You can check your current Ruby version using $Ruby -v
+HEREDOC
+
+if RUBY_VERSION < "2.5.0"
+    raise StandardError.new("error_message") 
+end
+
 #read and map file to an array of lines
-lines = File.foreach('text to convert.txt').map { |line| line.chomp }
+lines = File.foreach('text_to_convert.txt').map { |line| line.chomp }
 output = lines.map { |line| "      \"" + line + "\"," }
 output.last.delete_suffix!(",")
 
@@ -14,4 +25,4 @@ output.push("		\"description\": \"<-- description of what it does -->\"")
 output.push("  }")
 
 #write file
-File.write("converted text.txt", output.join("\n"))
+File.write("converted_text.txt", output.join("\n"))
